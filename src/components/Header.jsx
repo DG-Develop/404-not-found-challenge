1dd16ebcaa89fb4ae8e8e826aa42d8e88b12f634
DG-Develop <dgdband@hotmail.com>
2020-09-18 23:38:51 -0500

    Inicio del proyecto y agregando caracteristicas

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+    <header className="Header">
+        <label className="Header__title"> 404 Not Found</label>
+    </header>
+)
+
+export default Header
---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NotFound from "../pages/NotFound";
+
+import '../../global.scss'
+
+const App = () =>  (
+    <React.Fragment>
+        <NotFound />
+    </React.Fragment>
+  );
+
+export default App;
